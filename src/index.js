7af5cf2696babe3d57c9593326ba65006e793594
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-05-20 14:11:35 -0400

    logic for basic player movement done

--- src/index.js
+++ src/index.js
@@ -11,3 +11,73 @@ import { SocialLink } from './classes/socialLink'
 canvas.height = innerHeight
 canvas.width = innerWidth
 
+// player and its movement
+const player1 = new Player()
+
+// controller used in listen events to update x-axis velocity
+const right = {
+    isPressed: false
+}
+const left = {
+    isPressed: false
+}
+
+// listen to events and update player controller values
+addEventListener('keydown', (event) => {
+
+    switch(event.key) {
+
+        case 'a' || 'A':
+            left.isPressed = true; break;
+
+        case 'd' || 'D':
+            right.isPressed = true; break;
+
+        case 'w' || 'W' && !event.repeat:
+            if (player1.velocity.y === 0) {
+
+                player1.velocity.y -= 20; break;
+
+            }
+    }
+})
+
+// used to prevent velocity update when key isn't pressed
+addEventListener('keyup', ({key}) => {
+    
+    switch(key) {
+        case 'a' || 'A':
+            left.isPressed = false; break;
+            
+        case 'd' || 'D':
+            right.isPressed = false; break;
+
+
+    }
+})
+
+
+
+// play the game -- animate canvas
+const play = () => {
+    // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player1.update(ctx)
+
+    // update velocity based on events
+    if (right.isPressed) {
+        player1.velocity.x = 5
+    } else {
+        player1.velocity.x = 0
+    }
+
+    if (left.isPressed) {
+        player1.velocity.x = -5
+    } 
+
+    
+
+    requestAnimationFrame(play)
+}
+
+play()
