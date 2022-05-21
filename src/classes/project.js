a28263808498b94f40d1760290d423add49259d8
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-05-21 14:38:13 -0400

    + projects, refactor constructor, update/draw

--- src/classes/project.js
+++ src/classes/project.js
@@ -1,13 +1,65 @@
-export class Project {
+const projects = [
+    {
+        link: 'https://tyreeckcodes.com',
+        img: ''
+    },
+    {
+        link: 'https://mblog-api.herokuapp.com/',
+        img: ''
+    },
+    { 
+        link: 'https://reddit-miniapp.netlify.app/',
+        img: ''
+    }, 
+    { 
+        link: 'https://donutland2.netlify.app/',
+        img: ''
+    }, 
+    { 
+        link: 'https://the-sunnyside-agency.netlify.app/',
+        img: ''
+    }
+]
+
+export class Projects {
     // img == project screenshot, link == website link
-    constructor(img, link) {
-        this.img = img
-        this.link = link
+    constructor(x, y) {
+        this.projects = projects
+
+        // projects position
+        this.position = {
+            x,
+            y
+        }
+
+        // dimensions 
+        this.dimensions = {
+            height: 150,
+            width: 300
+        }
+
+        // current project index
+        this.currentProjectIndex = 0
     }
 
     // draw it
     // ctx == canvas context
     draw(ctx) {
+        ctx.fillStyle = 'blue'
+        ctx.fillRect(this.position.x, this.position.y, 300, 150)
+        ctx.fillStyle = 'black'
+    }
 
+    // update
+    // ctx == canvas context
+    update(ctx) {
+        setTimeout(() => {
+            if (this.currentProjectIndex === this.projects.length - 1) {
+                this.currentProjectIndex = 0
+            } else {
+                this.currentProjectIndex += 1
+            }
+        }, 2500)
+        this.draw(ctx)
     }
 }
